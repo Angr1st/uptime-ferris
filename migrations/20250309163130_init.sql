82a7d425dde6d2e5787abfeb485c4988f7e921b0
Torben Clasen <t.clasen@outlook.com>
2025-03-09 18:51:32 +0100

    Add migration; add sqlite & postgres; add clap

---
+++ migrations/20250309163130_init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS Websites (
+    id serial primary key,
+    url varchar not null,
+    alies varchar(75) not null unique
+);
+
+CREATE TABLE IF NOT EXISTS Logs (
+    id serial primary key,
+    website_id int NOT null REFERENCES Websites(id),
+    status smallint,
+    created_at timestamp with time zone not null default date_trunc('minute', current_timestamp),
+    UNIQUE(website_id, created_at)
+)
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,65 @@
-use axum::{Router, middleware::map_request_with_state, response::Html, routing::get};
+use axum::{Router, response::Html, routing::get};
+use clap::Parser;
+use sqlx::{PgPool, SqlitePool};
 use tokio::time::{Duration, sleep};
+
+/// Configure either Postgres or Sqlite connection string
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Postgres Db Connection String
+    #[arg(short, long, env, default_value = None)]
+    pg: Option<String>,
+
+    /// Sqlite Db
+    #[arg(short, long, env, default_value_t = true)]
+    sqlite: bool,
+}
+
+#[derive(Clone)]
+enum AppState {
+    Postgres(PgPool),
+    Sqlite(SqlitePool),
+}
+
+impl AppState {
+    fn new(postgres: Option<PgPool>, sqlite: Option<SqlitePool>) -> Self {
+        match (postgres, sqlite) {
+            (Some(p), _) => AppState::Postgres(p),
+            (_, Some(s)) => AppState::Sqlite(s),
+            _ => panic!("You need to configure either Postgres or Sqlite!"),
+        }
+    }
+}
+
+const SQLITE_CONNECTION_STRING: &'static str = "sqlite:uptime_ferris.db";
+
+impl AppState {
+    async fn from(item: Args) -> Self {
+        if let Some(pg_string) = item.pg {
+            if pg_string.is_empty() {
+                AppState::new(
+                    None,
+                    Some(SqlitePool::connect(SQLITE_CONNECTION_STRING).await.unwrap()),
+                )
+            } else {
+                AppState::new(Some(PgPool::connect(&pg_string).await.unwrap()), None)
+            }
+        } else {
+            AppState::new(
+                None,
+                Some(SqlitePool::connect(SQLITE_CONNECTION_STRING).await.unwrap()),
+            )
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
+    let app_state = AppState::from(args).await;
     // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new().route("/", get(handler).with_state(app_state));
 
     let calling_myself = tokio::spawn(calling_myself());
     // run it
