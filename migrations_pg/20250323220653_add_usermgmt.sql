3ee26638f45bb023de103e90a4732524c2806367
Torben Clasen <t.clasen@outlook.com>
2025-03-23 23:55:50 +0100

    start adding additional tables for user management

---
+++ migrations_pg/20250323220653_add_usermgmt.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS Users (
+    id serial PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    salt TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS Permissions (
+    id serial PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS User_Permissions (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    website_id INTEGER NOT NULL,
+    permission_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE,
+    FOREIGN KEY (website_id) REFERENCES Websites (id) ON DELETE CASCADE,
+    FOREIGN KEY (permission_id) REFERENCES Permissions (id),
+    UNIQUE (user_id, website_id, permission_id)
+);
+
+-- Insert the two permission types
+INSERT INTO Permissions (name, description) VALUES 
+    ('read', 'Can read the Website'),
+    ('create_modify', 'Can create and modify the Website');
