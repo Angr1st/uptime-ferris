62bc7d90578549a02d040dab46e4e2f5bdaa7564
Torben Clasen <t.clasen@outlook.com>
2025-03-23 22:17:43 +0100

    Add graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,10 @@ use futures_util::StreamExt;
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
 use sqlx::{PgPool, SqlitePool, migrate::Migrator};
-use tokio::time::{self, Duration};
+use tokio::{
+    signal,
+    time::{self, Duration},
+};
 use tower_http::trace::TraceLayer;
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
@@ -189,7 +192,10 @@ async fn main() {
         .await
         .unwrap();
     info!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
 }
 
 async fn styles() -> impl AxumIntoResponse {
@@ -525,5 +531,29 @@ async fn check_websites_sqlite(db: SqlitePool) {
                 .await
                 .unwrap();
         }
+    }
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
     }
 }
