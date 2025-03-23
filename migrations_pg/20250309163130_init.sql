521c9f031aee85c12994c5ce2d17e8dfdd2d8a71
Torben Clasen <t.clasen@outlook.com>
2025-03-23 23:55:13 +0100

    no timezone info -> save with utc explictly -> todo

--- migrations_pg/20250309163130_init.sql
+++ migrations_pg/20250309163130_init.sql
@@ -1,13 +1,14 @@
 CREATE TABLE IF NOT EXISTS Websites (
     id serial primary key,
     url varchar not null,
-    alias varchar(75) not null unique
+    alias varchar(75) not null unique,
+    created_at timestamp without time zone not null default current_timestamp
 );
 
 CREATE TABLE IF NOT EXISTS Logs (
     id serial primary key,
     website_id int NOT null REFERENCES Websites(id),
     status smallint,
-    created_at timestamp with time zone not null default date_trunc('minute', current_timestamp),
+    created_at timestamp without time zone not null default date_trunc('minute', current_timestamp),
     UNIQUE(website_id, created_at)
 );
