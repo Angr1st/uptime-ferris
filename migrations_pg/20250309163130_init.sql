3905c593ad810a67175b7abac50f08f4769b0c51
Torben Clasen <t.clasen@outlook.com>
2025-03-24 21:58:33 +0100

    Add created and updated wip

--- migrations_pg/20250309163130_init.sql
+++ migrations_pg/20250309163130_init.sql
@@ -2,13 +2,17 @@ CREATE TABLE IF NOT EXISTS Websites (
     id serial primary key,
     url varchar not null,
     alias varchar(75) not null unique,
-    created_at timestamp without time zone not null default current_timestamp
+    created_at timestamp without time zone not null,
+    created_at_user timestampz not null,
+    updated_at timestamp without time zone not null,
+    updated_at_user timestampz not null,
 );
 
 CREATE TABLE IF NOT EXISTS Logs (
     id serial primary key,
     website_id int NOT null REFERENCES Websites(id),
     status smallint,
+    error_msg varchar(MAX) NOT NULL,
     created_at timestamp without time zone not null default date_trunc('minute', current_timestamp),
     UNIQUE(website_id, created_at)
 );
